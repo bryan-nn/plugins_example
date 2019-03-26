7e6981b922d09317d278572e9aa9a579ae0b65b3
homholueng <homholueng@gmail.com>
2019-03-26 12:22:15 +0800

    Update test1.py

--- custom_plugins/components/col/test1.py
+++ custom_plugins/components/col/test1.py
@@ -7,7 +7,7 @@ from pipeline.conf import settings
 from pipeline.core.flow.activity import Service, StaticIntervalGenerator
 from pipeline.component_framework.component import Component
 
-__group_name__ = _(u"自定义原子(CUS)")
+__group_name__ = u"自定义原子(CUS)"
 
 
 class Pause1Service(Service):
