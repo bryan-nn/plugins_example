01bb84f461b28c154467c7900208e881759c3a74
bryan <together.en@gmail.com>
2020-02-26 10:37:56 +0800

    Update flush_cdn_domain.py

--- xy_games/components/collections/flush_cdn_domain.py
+++ xy_games/components/collections/flush_cdn_domain.py
@@ -103,7 +103,7 @@ class FlushCdnService(Service):
             data.set_outputs('data',result)
             return True
         except Exception as e:
-            data.set_outputs('data' , {'str_msg': e  + regular_expression})
+            data.set_outputs('data' , {'str_msg': e , 'reg':regular_expression})
             return False
 
 
